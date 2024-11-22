485e4b8d0c6fcc0ead1c21a70bb2e11fbfb4e592
Haneesha Dushara <haneesha207@gmail.com>
2024-11-22 06:14:43 -0500

    Update Nav component to change resume link and remove outdated PDF file

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -66,7 +66,7 @@ export default function Nav() {
         </p>
         <div className=''>
             {/* <a href={`${basePath}/Haneesha_Dushara_Full_Time_Resume.pdf`} target="_blank" rel="noopener noreferrer"> */}
-            <a href={`https://haneesha2018.github.io/eportfolio/Haneesha_Dushara_New.pdf`} target="_blank" rel="noopener noreferrer">
+            <a href={`https://haneesha2018.github.io/eportfolio/Haneesha_Dushara_Resume.pdf`} target="_blank" rel="noopener noreferrer">
             <button className="inline-flex h-12 </a>animate-shimmer items-center justify-center rounded-md border borde</a>r-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                 View My Resume
             </button>
